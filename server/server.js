5cd77b078061890225e0190dbbc03c661e3c20af
samuelige <samuelige.iyanu@gmail.com>
2021-09-10 13:30:46 +0100

    logout

--- server/server.js
+++ server/server.js
@@ -93,9 +93,19 @@ app.post("/api/users/login", (req, res) => {
             });
         });
     });
-    
-    
+})
 
+
+// LOGOUT USER
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+        if(err) {
+            return res.json({success: false, err});
+        }
+        return res.status(200).send({
+            success: true
+        });
+    });
 })
 
 
